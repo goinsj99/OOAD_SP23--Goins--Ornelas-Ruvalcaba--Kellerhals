bbbde14a8dc6f156ec8039f5e4ac4862c6a5e5e4
goinsj99 <82783205+goinsj99@users.noreply.github.com>
2023-02-16 09:32:47 -0700

    added an override for the staff positions

--- Staff.java
+++ Staff.java
@@ -2,113 +2,129 @@ import java.util.Random;
 import java.util.ArrayList;
 import java.util.*;
 import java.util.List;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.*;
+import java.util.List;
 import java.util.UUID;
 
 
-    public class Staff {
-        private Double salaryRate;
-        private Integer staffID;
-        private Double salary;
-        private Double hours;
-        private String position;
-        private String firstName;
-        private Double bonus;
-        private Float Rate;
+public class Staff {
+    private Double salaryRate;
+    private Integer staffID;
+    private Double salary;
+    private Double hours;
+    private String position;
+    private String firstName;
+    private Double bonus;
 
-        public Staff(String firstName, Double salaryRate, Integer staffID, Double salary, Double hours, String position, Double bonus) {
-            this.firstName = firstName;
-            this.salaryRate = salaryRate;
-            this.staffID = staffID;
-            this.hours = hours;
-            this.position = position;
-            this.salary = salary;
-            this.bonus = bonus;
-        }
+    private Float Rate;
 
-        public void setName(String firstName) {
-            this.firstName = firstName;
-        }
+    public Staff(String firstName, Double salaryRate, Integer staffID, Double salary, Double hours, String position, Double bonus) {
+        this.firstName = firstName;
+        this.salaryRate = salaryRate;
+        this.staffID = staffID;
+        this.hours = hours;
+        this.position = position;
+        this.salary = salary;
+        this.bonus = bonus;
+    }
 
-        public void setSalaryRate(Double salaryRate) {
-            this.salaryRate = salaryRate;
-        }
 
-        public void setStaffID(Integer staffID) {
-            this.staffID = staffID;
-        }
+    public void setName(String firstName) {
+        this.firstName = firstName;
+    }
 
-        public void setHours(Double hours) {
-            this.hours = hours;
-        }
+    public void setSalaryRate(Double salaryRate) {
+        this.salaryRate = salaryRate;
+    }
 
-        public void setPosition(String position) {
-            this.position = position;
-        }
+    public void setStaffID(Integer staffID) {
+        this.staffID = staffID;
+    }
 
-        public void setSalary(Double Salary) {
-            this.salary = Salary;
-        }
-        public void setBonus(Double bonus){this.bonus = bonus;}
-        public String getName() {
-            return firstName;
-        }
+    public void setHours(Double hours) {
+        this.hours = hours;
+    }
 
-        public Double getSalaryRate() {
-            return salaryRate;
-        }
+    public void setPosition(String position) {
+        this.position = position;
+    }
 
-        public Integer getStaffID() {
-            return staffID;
-        }
+    public void setSalary(Double Salary) {
+        this.salary = salary;
+    }
+    public void setBonus(Double bonus){this.bonus = bonus;}
+    public String getName() {
+        return firstName;
+    }
 
-        public Double getHours() {
-            return hours;
-        }
+    public Double getSalaryRate() {
+        return salaryRate;
+    }
 
-        public String getPosition() {
-            return position;
-        }
+    public Integer getStaffID() {
+        return staffID;
+    }
 
-        public Double getSalary() {
-            return salary;
-        }
-        public Double getBonus(){return bonus;}
+    public Double getHours() {
+        return hours;
+    }
 
+    public String getPosition() {
+        return position;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+    public Double getBonus(){return bonus;}
+
+
+}
+
+// Subclass (inherit from Animal)
+class SalesPerson extends Staff{
+    Double myRate = 1.25;
+    private String position;
+    public SalesPerson(String firstName, Double salaryRate, Integer staffID, Double salary, Double hours, String position, Double bonus) {
+        super(firstName, salaryRate, staffID, salary, hours, position, bonus);
+        this.position = "Sales";
+    }
+    @Override
+    public String getPosition(){return position; }
+    public void myRate() {
 
     }
 
-    // Subclass (inherit from Animal)
-    class SalesPerson extends Staff{
-        Double myRate = 1.25;
+}
 
-        public SalesPerson(String firstName, Double salaryRate, Integer staffID, Double salary, Double hours, String position, Double bonus) {
-            super(firstName, salaryRate, staffID, salary, hours, position, bonus);
-        }
-
-        public void myRate() {
-
-        }
+class Intern extends Staff {
+    Double myRate = 1.5;
+    private String position;
+    public Intern(String firstName, Double salaryRate, Integer staffID, Double salary, Double hours, String position, Double bonus) {
+        super(firstName, salaryRate, staffID, salary, hours, position, bonus);
+        this.position = "Intern";
+    }
+    @Override
+    public String getPosition(){return position; }
+    public void myRate() {
 
     }
 
-    class Intern extends Staff {
-        Double myRate = 1.5;
-        public Intern(String firstName, Double salaryRate, Integer staffID, Double salary, Double hours, String position, Double bonus) {
-            super(firstName, salaryRate, staffID, salary, hours, position, bonus);
-        }
-        public void myRate() {
+}
 
-        }
+class Mechanic extends Staff {
+    Double myRate = 1.75;
+    private String position;
+    public Mechanic(String firstName, Double salaryRate, Integer staffID, Double salary, Double hours, String position, Double bonus) {
+        super(firstName, salaryRate, staffID, salary, hours, position, bonus);
+        this.position = "Mechanic";
+    }
+    @Override
+    public String getPosition(){return position; }
+    public void myRate() {
 
     }
 
-    class Mechanic extends Staff {
-        Double myRate = 1.75;
-        public Mechanic(String firstName, Double salaryRate, Integer staffID, Double salary, Double hours, String position, Double bonus) {
-            super(firstName, salaryRate, staffID, salary, hours, position, bonus);
-        }
-        public void myRate() {
-
-        }
-
-    }
+}
